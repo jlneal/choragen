2c5108a164dae91ee72ecc2b4eb2ad7a5465791c
Justin Neal <justin.neal@gmail.com>
2025-12-08 22:20:25 -0600

    chore: add .next to gitignore and remove build artifacts
    [CR-20251208-002]

--- packages/web/.next/static/chunks/pages/_app-083fa84d19d49a21.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{8567:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(2148)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(8567),_(6843)}),_N_E=n.O()}]);
